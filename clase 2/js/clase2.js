dcd70ad8fb8b1a64c3a25951f6e68a7c1f3097b2
luisNavasArg <lpipnavas@gmail.com>
2019-09-03 22:08:06 -0300

    Nueva clase

--- clase 2/js/clase2.js
+++ clase 2/js/clase2.js
@@ -1,35 +1,42 @@
-//Template literalrs
+// //Template literalrs
 
-//de la vieja forma
+// //de la vieja forma
 let cartas = document.getElementById('principal');
 const colores = ["green", "yellow", "red", "blue", "black"];
 const cantCartas = 108;
 const cantColor = 25;
 let aux = 0;
+let cartasDisponibles=0;
 const cartEspeciales = ["+2", "&#216;", "<-- -->"];
 
-for (let i = 0; i < cantCartas; i++) {
-    if (aux <= 9) {
-        cartas.innerHTML += "<div style='background-color:" + colores[parseInt(i / 25)] + ";' class='siz'>" + aux + "</div>";
-
-    }
-    if (aux == 10) {
-        aux = 0;
-    }
-    if (i == 20) {
-        for (let x = 0; x < 2; x++) {
-            for (let a = 0; a < cartEspeciales.length; a++) {
-                cartas.innerHTML += "<div style='background-color:" +
-                    colores[parseInt(i / 25)] + ";' class='siz'>" +
-                    cartEspeciales[a] + "</div>";
-            }
-        }
-
-    }
-    if (i == 24) {
-        cartas.innerHTML +=
-            "<div style='background-color:white;' class='siz'></div>";
-    }
-    aux++;
-
+//funciones flechas
+// function agregarCartas(){}
+const agregarCartas=(color,numero,otra)=>{
+    otra.innerHTML+=`
+        <div style='background-color:${color};' 
+        class='siz'>${numero}</div>
+    `;
 }
+
+const agregarEspeciales=(cartEspeciales,color,otra)=>{
+    cartas.innerHTML+=`
+    <div style='background-color:${color};' class='siz'>${cartEspeciales}</div>
+    `;
+}
+
+for(let a=0; a < cantCartas; a++){
+    agregarCartas(colores[parseInt(a/cantColor)],aux,cartas);
+    if(aux < 10){
+         aux++;
+    }
+    if(aux==10){
+        aux=1;
+    }
+    if(a > 18 && a < 26){
+        for(let x=0; x < cartEspeciales.length; x++){
+            agregarEspeciales(cartEspeciales[x],colores[parseInt(a/cantColor)],cartas);
+        }
+    }
+ 
+}
+
